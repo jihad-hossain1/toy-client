1dbcd60b182dd8946e37ea526bfc554643099d9e
jihad-hossain1 <jihadkham934@gmail.com>
2023-05-18 13:05:19 +0600

    home section add a banner

--- src/layouts/main/Main.jsx
+++ src/layouts/main/Main.jsx
@@ -6,14 +6,16 @@ import Footer from "../../components/footer/Footer";
 const Main = () => {
   return (
     <div>
-      <div className="p-2 md:max-w-[1000px] mx-auto">
+      <div className="p-2 md:max-w-[1200px] mx-auto">
         <Navbar></Navbar>
       </div>
       <hr className="border-[] border-gray-300 my-2" />
       <div className="min-h-screen">
         <Outlet></Outlet>
       </div>
-      <Footer></Footer>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import img1 from "../../assets/svg/Cloudy.svg";
+// import img2 from '../../assets/svg/babyBanner-removebg-preview.png'
 const Home = () => {
-  return <div>Home here</div>;
+  return (
+    <>    
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage: `url(${img1})`,
+          }}
+        >
+          <div className="hero-overlay bg-opacity-20"></div>
+          <div className="hero-content text-center text-white">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">
+                Best Kids Toys Store & Online Shop
+              </h1>
+              <p className="mb-5">Give The Gift Of Your Children Everyday</p>
+              <button className="btn btn-primary btn-outline">Get Started</button>
+            </div>
+          </div>
+        </div>
+    </>
+  );
 };
 
 export default Home;
